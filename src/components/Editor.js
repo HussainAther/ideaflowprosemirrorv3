9277f25e0349a01c556c0418ff93fc44a8179816
Syed Hussain Ather <shussainather@gmail.com>
2024-09-27 04:09:01 -0400

    editor shennignags showing

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -34,14 +34,5 @@ const Editor = () => {
   return <div ref={editorRef} className="editor"></div>;
 };
 
-const state = EditorState.create({
-  schema,
-  plugins: [
-    keymap({ "Mod-z": history.undo, "Mod-y": history.redo }),
-    history(),
-    autocompletePlugin  // Ensure the plugin is included here
-  ],
-});
-
 export default Editor;
 
